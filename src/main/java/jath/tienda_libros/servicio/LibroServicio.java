337307fa642c454466358932bed7578d209c5bb2
alexhDev2808 <colores.2808.8000@gmail.com>
2024-04-28 09:55:06 -0600

    Integracion de Swing y Spring

--- src/main/java/jath/tienda_libros/servicio/LibroServicio.java
+++ src/main/java/jath/tienda_libros/servicio/LibroServicio.java
@@ -3,9 +3,11 @@ package jath.tienda_libros.servicio;
 import jath.tienda_libros.modelo.Libro;
 import jath.tienda_libros.repositorio.LibroRepositorio;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
 
+@Service
 public class LibroServicio implements ILibroServicio {
 
     @Autowired
---
+++ src/main/java/jath/tienda_libros/vista/LibroForm.java
@@ -0,0 +1,35 @@
+package jath.tienda_libros.vista;
+
+import jath.tienda_libros.servicio.LibroServicio;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import javax.swing.*;
+import java.awt.*;
+
+@Component
+public class LibroForm extends JFrame {
+
+    LibroServicio libroServicio;
+    private JPanel panel;
+
+    @Autowired
+    public LibroForm(LibroServicio libroServicio){
+        this.libroServicio = libroServicio;
+        iniciarForma();
+    }
+
+    private void iniciarForma() {
+        setContentPane(panel);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setVisible(true);
+        setSize(900, 700);
+
+        // centrar pantalla
+        Toolkit toolkit = Toolkit.getDefaultToolkit();
+        Dimension tamPantalla = toolkit.getScreenSize();
+        int x = (tamPantalla.width - getWidth() / 2);
+        int y = (tamPantalla.height - getHeight() / 2);
+        setLocation(x, y);
+    }
+}
